9efc6dcd7a248a68afd07f5f0d90654780b4f7c0
dinoplecko@yahoo.com <dinoplecko@yahoo.com>
2024-06-29 16:47:03 +0200

    base for normalizing input json

--- scripts/modules/globals.js
+++ scripts/modules/globals.js
@@ -3,7 +3,7 @@ export { selected, gameData, categoryByElement};
 
 
 const selected = new Set();
-const gameData = {};
+let gameData = {};
 
 
 const onGameDataLoaded = new Event("gamedataloaded");
@@ -12,9 +12,8 @@ await fetch('data/game2.json').then(async response => {
    // update so reference passed to buttonFunctions doesn't break
    // TODO: gamedata = data? mayb
    await response.json().then((data) => {
-		for (let key in data) {
-         gameData[key] = data[key];
-      }
+      normalizeFormat(data);
+		gameData = data;
    });
 
 }).then(() => {
@@ -22,6 +21,15 @@ await fetch('data/game2.json').then(async response => {
 		window.dispatchEvent(onGameDataLoaded);
 	}, 50);
 });
+
+// alters data
+function normalizeFormat(data) {
+   data.initial = data.initial.map(row => row.map(title => title.toUpperCase()));
+   //data.categories.forEach(c => )
+   // TODO: implement format assertions (with assert?)
+}
+
+
 
 
 const elementCategory = Object.fromEntries(
