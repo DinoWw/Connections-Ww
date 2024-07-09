acc525c5f044e23de45162e9f9f8943e59ed1e1d
DinoWw <dinoplecko@yahoo.com>
2024-07-09 16:51:27 +0200

    path import

--- connections.routes.js
+++ connections.routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const fs = require('fs')
-
+const path = require("path")
 
 router.use(express.static(path.join(__dirname, 'public')));
 
